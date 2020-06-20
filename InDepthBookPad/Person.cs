179aba4405be0c5a4cceeeca031ab7dc3d87c751
Benjamin Boyce <boyceb@gmail.com>
2020-06-20 12:09:17 -0700

    Add project files.

---
+++ InDepthBookPad/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InDepthBookPad
+{
+    // <summary>
+    /// A class to define a person
+    /// </summary>
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
